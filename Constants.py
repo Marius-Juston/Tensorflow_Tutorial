2e09b98c9db795b701c9fac0140fccd0400f37be
NamelessSuperCoder <Marius.juston@hotmail.fr>
2017-10-16 18:48:07 -0400

    Initial commit

---
+++ Constants.py
@@ -0,0 +1,17 @@
+# The canonical import statement for TensorFlow programs is as follows:
+
+import tensorflow as tf
+# This gives Python access to all of TensorFlow's classes, methods, and symbols. Most of the documentation assumes you have already done this.
+# One type of node is a constant. Like all TensorFlow constants, it takes no inputs, and it outputs a value it stores internally
+node1 = tf.constant(3.0, dtype=tf.float32)
+node2 = tf.constant(4.0) # also tf.float32 implicitly
+print(node1, node2)
+
+# The final print statement produces
+# Tensor("Const:0", shape=(), dtype=float32) Tensor("Const_1:0", shape=(), dtype=float32)
+
+sess = tf.Session()
+print(sess.run([node1, node2]))
+
+# we see the expected values of 3.0 and 4.0:
+# [3.0, 4.0]
---
+++ Tensors.py
@@ -0,0 +1,4 @@
+tensor0 = 3 # a rank 0 tensor; this is a scalar with shape []
+tensor1 = [1., 2., 3.] # a rank 1 tensor; this is a vector with shape [3]
+tensor2 = [[1., 2., 3.], [4., 5., 6.]] # a rank 2 tensor; a matrix with shape [2, 3]
+tensor3 = [[[1., 2., 3.]], [[7., 8., 9.]]] # a rank 3 tensor with shape [2, 1, 3]
